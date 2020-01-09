a6091b8daf9c5fb6af53cb9dc97a1ba4ebb36c77
dfiredeveloper <dfiredeveloper@gmail.com>
2020-01-09 13:51:55 +0100

    update app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-     navigator.serviceWorker.register('../sw.js').then( () => {
-      console.log('Service Worker Registered')
-     })
+  window.addEventListener('load', () => {
+   navigator.serviceWorker.register('../sw.js').then( () => {
+    console.log('Service Worker Registered')
    })
-  }
+ })
+}
